c15fc41685839fb149556072098ee29e89e008c1
JJKKJJKJ <spergals950814@gmail.com>
2025-03-10 18:03:53 +0900

    태스트

--- SplitSeconds/src/main/java/com/qwer/module/codeGroup/CodeGroupController.java
+++ SplitSeconds/src/main/java/com/qwer/module/codeGroup/CodeGroupController.java
@@ -1,5 +1,14 @@
 package com.qwer.module.codeGroup;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class CodeGroupController {
 
+	@RequestMapping(value = "/codegroup/CodeGroupXdmList")
+	public String codegroupXdmList( ) {
+		
+		return "xdm/codegroup/CodeGroupXdmList";
+	}
 }
