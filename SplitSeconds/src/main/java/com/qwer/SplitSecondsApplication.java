883d6f73b3252843542ba7ddffebe987cf5daa30
JJKKJJKJ <spergals950814@gmail.com>
2025-03-10 11:00:12 +0900

    first

---
+++ SplitSeconds/src/main/java/com/qwer/SplitSecondsApplication.java
@@ -0,0 +1,13 @@
+package com.qwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitSecondsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitSecondsApplication.class, args);
+	}
+
+}
---
+++ SplitSeconds/src/test/java/com/qwer/SplitSecondsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitSecondsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
