e21d631fd023cfb8de5be2e9483ab8fe5cd9e380
JJKKJJKJ <spergals950814@gmail.com>
2025-04-14 13:03:05 +0900

    ㅁㅁ버 왜 안되지

--- SplitSeconds/src/main/java/com/qwer/module/member/MemberController.java
+++ SplitSeconds/src/main/java/com/qwer/module/member/MemberController.java
@@ -5,38 +5,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.qwer.module.code.CodeDto;
-import com.qwer.module.codeGroup.CodeGroupVo;
 @Controller
 public class MemberController {
 	@Autowired
 	MemberService memberService;
-	
-	
-	@RequestMapping(value = "/xdm/member/MemberXdmList")
-	public String MemberXdmList(@ModelAttribute("vo") MemberVo vo , Model model) {
-//		
-//		vo.setParamsPaging(memberService.selectOneCount());
-//	
-//		model.addAttribute("list",memberService.selectList(vo));
 
+	
+	
+	@RequestMapping(value = "MemberService memberService;")
+	public String memberList(@ModelAttribute("vo") MemberVo vo , Model model) {
+		
+		vo.setParamsPaging(memberService.selectOneCount());
+	
+//		int a = codeGroupService.selectOneCount();
+		model.addAttribute("list",memberService.selectList(vo));
+//		model.addAttribute("vo",vo);
 		
 		return "/xdm/member/MemberXdmList";
 	}
-	
-	
-	
+//	
 //	@RequestMapping(value = "/xdm/member/MemberXdmForm")
-//	public String codegroupXdmForm(CodeGroupVo codeGroupVo , Model model) { 
-//		model.addAttribute("list",codeGroupService.selectListForCodeSelect(codeGroupVo));
-//		return "xdm/member/MemberXdmForm";
-////	}
-//	@RequestMapping(value = "/member/MemberXdmInst")
-//	public String memberXdmInst(MemberDto memberDto) 
-//		
-//		memberService.insert();
-//		return "redirect:/member/MemberXdmList";
+//	public String MemberXdmForm(MemberVo codeGroupVo , Model model) { 
+//		model.addAttribute("list",memberService.selectListForMemberSelect(MemberVo));
+//		return "/xdm/member/MemberXdmForm";
 //	}
+	@RequestMapping(value = "/member/MemberXdmInst")
+	public String memberInst(MemberDto memberDto) {
+		
+		memberService.insert(memberDto);
+		return "redirect:/xdm/member/MemberXdmList";
+	}
 	
 }
